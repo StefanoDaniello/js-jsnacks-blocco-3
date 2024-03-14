4e4f39a8a8607ef9414aef8cbcd28a86f9cf22de
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-14 11:59:24 +0100

    first snack

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+/*
+let array =[];
+
+let sommma = 0;
+
+while ( sommma < 50){
+    let userNumber=parseInt(prompt('inserisci un numero'));
+    if(!isNaN(userNumber)){
+        sommma += userNumber;
+        array.push(userNumber);
+    }
+}
+console.log(array,sommma)
+*/
