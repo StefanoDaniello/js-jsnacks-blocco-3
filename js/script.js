b3229ec5180e0ade8caa8a3bf4743f28279becc1
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-14 12:14:49 +0100

    add random number on array

--- js/script.js
+++ js/script.js
@@ -15,3 +15,26 @@ while ( sommma < 50){
 }
 console.log(array,sommma)
 */
+
+
+/**
+ * Snack3
+    Fai inserire un numero, che chiameremo N, all'utente.
+    Genera con una funzione N array, ognuno formato da 10 numeri casuali da 1 a 100.
+    Ogni volta che ne crei uno, stampalo.
+ */
+
+
+let N =parseInt(prompt('inserisci un numero'));
+
+let array=[];
+
+for(i=0 ; i< 10; i++){
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+   let randomNumber= getRndInteger(1, 100)
+   array.push(randomNumber)
+}
+console.log(array)
+
